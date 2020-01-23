23181f4516421f0aa5cc7208c6c42b78cb7da7b1
Marwan-Amin <marwanamin095@gmail.com>
2020-01-23 19:18:23 +0200

    models created and migrations modified

--- database/migrations/2020_01_23_141130_create_user_income_table.php
+++ database/migrations/2020_01_23_141130_create_user_income_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersSubExpensesTable extends Migration
+class CreateUserIncomeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateUsersSubExpensesTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_sub_expenses', function (Blueprint $table) {
+        Schema::create('user_income', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->unsignedBigInteger('sub_category_id');
+            $table->unsignedBigInteger('income_id');
             $table->decimal('amount',8,2);
             $table->timestamps();
         });
@@ -30,6 +30,6 @@ class CreateUsersSubExpensesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_sub_expenses');
+        Schema::dropIfExists('user_income');
     }
 }
