331264379ce7afa73bbeb4b8254bd8a11617bfc8
Marwan-Amin <marwanamin095@gmail.com>
2020-01-24 16:26:03 +0200

    layout template for dashboard only modified

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,6 +90,12 @@
                   <i class="mdi mdi-logout mr-2 text-primary"></i>  {{ __('Logout') }} </a>
               </div>
             </li>
+
+            <li class="nav-item  full-screen-link">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                  <i class="mdi mdi-logout mr-2 text-primary"></i>  {{ __('Logout') }} </a>
+                  </li>
+                  
             <li class="nav-item d-none d-lg-block full-screen-link">
               <a class="nav-link">
                 <i class="mdi mdi-fullscreen" id="fullscreen-button"></i>
