cc13fe86d577d3ec4ece12223d1a5e0adf046077
Amr Samy <amr.saami@gmail.com>
2020-01-27 23:03:11 +0200

    done  event manager with comments

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -91,45 +91,65 @@ $.ajax({
        }
     });  
 }
+
+//render the inputs needed for the event that user has entered
 function renderResponse(data){
     if(data.isStored){
     let eventActionContainer= document.getElementById('eventActionButtons');
     let editEvent = document.createElement('a');
-    let href = "{{route('events.edit',['id'=>':data.categoryId'])}}";
-        href = href.replace(':data.categoryId',data.categoryId);
         editEvent.innerHTML = 'Edit Event';
         editEvent.setAttribute('href',href);
+        //set the href for the edit anchor tag 
+    let href = "{{route('events.edit',['id'=>':data.categoryId'])}}";
+        href = href.replace(':data.categoryId',data.categoryId);
+        //create anchor tag for create new event
     let addEvent = document.createElement('a');
         addEvent.setAttribute('href',"{{route('events.create')}}"); 
         addEvent.innerHTML = "Add Event";
+        //append the anchor tags into the eventActionContainer Div
         eventActionContainer.appendChild(editEvent);
         eventActionContainer.appendChild(addEvent);
+        //create input and set event name into that input 
     let eventName = document.createElement('input');
         eventName.setAttribute('type','text');
+
         document.getElementById('subCategoryNameLabel').innerHTML = 'Event Expense';
+        
+        //create input and set event amount into that input 
     let eventSubCategoryAmount = document.createElement('input');
         eventSubCategoryAmount.setAttribute('type','number');
         eventSubCategoryAmount.setAttribute('step','0.01');
+
         document.getElementById('subCategoryAmountLabel').innerHTML ='Amount'; 
+       
+        //create input and set event date into that input 
     let eventSubCategoryDate = document.createElement('input');
         eventSubCategoryDate.setAttribute('type','date');
-        document.getElementById('subCategoryDateLabel').innerHTML ='Date'; 
+
+        document.getElementById('subCategoryDateLabel').innerHTML ='Date';
+      //create add custom sub expents button
     let addSubCategoryButton = document.createElement('button');
         addSubCategoryButton.innerHTML ="Add Sub-Expense";
+// append the rest of the elements
     let subCategoryNameParent = document.getElementById('subCategoryName');
         subCategoryNameParent.appendChild(eventName);
     let subCategoryAmountParent = document.getElementById('subCategoryAmount');
         subCategoryAmountParent.appendChild(eventSubCategoryAmount);
     let subCategoryDateParent = document.getElementById('subCategoryDate');
         subCategoryDateParent.appendChild(eventSubCategoryDate);
+
+        //disabled the input and add button of event after the user enter one to stop him of enter more than one event at a time
         document.getElementById('addEvent').disabled =true;
         document.getElementById('category').disabled = true;
         document.getElementById('buttonSubCategory').appendChild(addSubCategoryButton);
+
+        // this function will store custoSubCategory By Ajax and it will send data and elements needed to support this operation
         subCategoryAjax(addSubCategoryButton,eventName,eventSubCategoryAmount,eventSubCategoryDate,data.categoryId);
     }   
 }
 function subCategoryAjax(addButton,subName,amount,date,categoryId){
 addButton.addEventListener('click',function(){
+  //check if the three fields is full and not empty and if it is it will alert an error
     if(subName && amount && date){
 
         let urlSubCategory = `{{route('events.subStore')}}`;
@@ -142,7 +162,8 @@ addButton.addEventListener('click',function(){
   type: 'POST',
    data:subCategoryInfo,
        success:function(data){
-      createEventRecord(data);
+         //create information record about the event 
+      createEventInfoRecord(data);
        }
     }); 
     }else{
@@ -151,7 +172,7 @@ addButton.addEventListener('click',function(){
 });
 }
 });
-function createEventRecord(eventData){
+function createEventInfoRecord(eventData){
       let table_body = document.getElementById('event_table_body');
       let table_row = document.createElement("tr");
       let table_data_amount = document.createElement("td");
