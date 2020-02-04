ed647c652be9247e45d920b42f405f2a317573a6
ahmedhamam010 <ahmedhamam454546@gmail.com>
2020-02-04 17:40:41 +0200

    fix apdATE IMGE

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Country;
+use Auth;
+
 
 class ProfileController extends Controller
 {
@@ -21,18 +23,34 @@ class ProfileController extends Controller
 
     public function update(Request $request ,$id) 
     {
-        $path = request()->file('avatar')->store('upload');
         
-        request()->avatar->move(public_path('upload'), $path);
         $user = User::find($id);
-        $user->name = request()->name;
-        $user->email = request()->email;
-        $user->gender = request()->gender;
-        $user->age = request()->age;
-        $user->country = request()->country;
-        $user->city = request()->city;
-        $user->avatar = $path;
-        $user->save();
+        if( request()->avatar ){
+            $path = request()->file('avatar')->store('upload');
+            request()->avatar->move(public_path('upload'), $path);
+            $user->name = request()->name;
+            $user->email = request()->email;
+            $user->gender = request()->gender;
+            $user->age = request()->age;
+            $user->country = request()->country;
+            $user->city = request()->city;
+            $user->avatar = $path;
+            $user->save();
+        }else{
+           
+            $user->name = request()->name;
+            $user->email = request()->email;
+            $user->gender = request()->gender;
+            $user->age = request()->age;
+            $user->country = request()->country;
+            $user->city = request()->city;
+            $user->avatar = Auth::user()->avatar;
+            $user->save();
+
+
+        }
+        
+        
         return redirect('user_profile')->with('message', 'updated successfuly');
         
     }
