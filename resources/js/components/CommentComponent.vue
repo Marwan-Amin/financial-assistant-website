e778b7f35b95579bbf5a98ad10ba4500b2354cd1
Marwan-Amin <marwanamin095@gmail.com>
2020-02-06 17:02:14 +0200

    adding blog modifications

--- resources/js/components/CommentComponent.vue
+++ resources/js/components/CommentComponent.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+ <div class="row">
+ 
+ </div>
 </template>
 
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            console.log('Component mounted.');
+            Echo.join(`comments`)
+            .here((users)=>{
+                console.log(users);
+            }).joining((user)=>{
+                console.log(`${user.name} joined`)
+            }).leaving((user)=>{
+                console.log(`${user.name} left`)
+            })
         }
     }
 </script>
