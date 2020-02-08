a0a357dce3a796223ca78196d8c684267b903f42
ahmedhamam010 <ahmedhamam454546@gmail.com>
2020-02-08 15:14:41 +0200

    modify footer of home page

--- resources/views/layouts/homeLayout.blade.php
+++ resources/views/layouts/homeLayout.blade.php
@@ -115,40 +115,36 @@
         <div class="row mb-5">
           <div class="col-md">
             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Company</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+              <h2 class="ftco-heading-2">Application</h2>
+              <p>We bring together all of your income, bills and more, so you can conveniently manage your finances from one dashboard,.</p>
+              
             </div>
           </div>
-          <div class="col-md">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Quick Links</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Home</a></li>
-                <li><a href="#" class="py-2 d-block">About</a></li>
-                <li><a href="#" class="py-2 d-block">Services</a></li>
-                <li><a href="#" class="py-2 d-block">Portfolio</a></li>
-                <li><a href="#" class="py-2 d-block">Contact</a></li>
-                <li><a href="#" class="py-2 d-block">Privacy</a></li>
-              </ul>
-            </div>
-          </div>
+         
           <div class="col-md">
              <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Contact Information</h2>
               <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">198 West 21th Street, Suite 721 New York NY 10016</a></li>
-                <li><a href="#" class="py-2 d-block">+ 1235 2355 98</a></li>
-                <li><a href="#" class="py-2 d-block">info@yoursite.com</a></li>
-                <li><a href="#" class="py-2 d-block">email@email.com</a></li>
+                <li><a href="#" class="py-2 d-block">1 Mahmoud Said St., Shohada Square, Main Post Office Building, Alexandria</a></li>
+                <li><a href="#" class="py-2 d-block">+ 033906924/5/6</a></li>
+                <li><a href="#" class="py-2 d-block"> ITIinfo@iti.gov.eg</a></li>
+                
               </ul>
             </div>
           </div>
+
           <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Follow us</h2>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+                <li><a href="https://twitter.com/itichannel2"><span class="icon-twitter"></span></a></li>
+                <li><a href="https://www.facebook.com/Ask.iTi.gov"><span class="icon-facebook"></span></a></li>
+                <li><a href="https://www.linkedin.com/company/294614"><span class="icon-linkedin"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          
+          <!-- <div class="col-md">
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Newsletter</h2>
               <p>Far far away, behind the word mountains, far from the countries.</p>
@@ -159,7 +155,7 @@
                 </div>
               </form>
             </div>
-          </div>
+          </div> -->
         </div>
         <div class="row">
           <div class="col-md-12 text-center">
